2e8260e1f81e2d1cd1fdc9ec7c57d80447bd61b3
Archie Lee <archie_lee7@hotmail.com>
2017-05-11 23:47:22 -0500

    Added config script

---
+++ linux_configs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Turn off "Remote Search", so search terms in Dash don't get sent to the internet
+gsettings set com.canonical.Unity.Lenses remote-content-search none
+# If you're using a later version, disable remote scopes
+gsettings set com.canonical.Unity.Lenses disabled-scopes \
+      "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope',
+      'more_suggestions-populartracks.scope', 'music-musicstore.scope',
+      'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope',
+      'more_suggestions-skimlinks.scope']"
+echo "All done. Enjoy your privacy."
+
+# Chrome
+wget -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+# Sublime
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+
+echo "[Updating the packages]"
+sudo apt-get update
+
+echo "[Installing the packages]"
+sudo apt-get install -y arduino terminator openssh-server git vlc indicator-multiload indicator-cpufreq screen sublime-text-installer
+
+sudo apt-get -y purge thunderbird* pidgin* gwibber* rhythmbox* ubuntuone* empathy* totem*
+
+sudo apt-get -y dist-upgrade
+
+git config --global user.email "archie_lee7@hotmail.com"
+git config --global user.name "Archie Lee"
+
+# Make Sublime default
+sudo sed -i.bak 's/gedit.desktop/sublime-text.desktop/g' /usr/share/applications/defaults.list
+sudo sed -i.bak 's/gedit.desktop/sublime-text.desktop/g' /etc/gnome/defaults.list
+
+# Add user to dialout
+sudo adduser $USER dialout
+echo "Added yourself to dialout, make sure to logout to take effect."
