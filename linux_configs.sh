9f5d8327e840ad10b95a1f06f116a62b19400fe0
Archie Lee <archie_lee7@hotmail.com>
2017-05-18 22:35:51 -0400

    Removed chrome, don't think it works atm

--- linux_configs.sh
+++ linux_configs.sh
@@ -11,8 +11,8 @@ gsettings set com.canonical.Unity.Lenses disabled-scopes \
 echo "All done. Enjoy your privacy."
 
 # Chrome
-wget -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+# wget -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+# sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 # Sublime
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
