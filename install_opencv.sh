a0609a34661b1db34c89878265a9d9846216a952
Archie Lee <archie_lee7@hotmail.com>
2017-08-09 17:54:11 -0400

    Add opencv_contrib modules to installation

--- install_opencv.sh
+++ install_opencv.sh
@@ -12,7 +12,13 @@ done
 # Update system and install build tools
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install build-essential cmake pkg-config
+sudo apt-get install build-essential \
+                     cmake \
+                     pkg-config \
+                     libgtk2.0-dev \
+                     libavcodec-dev \
+                     libavformat-dev \
+                     libswscale-dev
 
 # Install virtual environment
 sudo pip3 install virtualenv virtualenvwrapper
@@ -32,13 +38,16 @@ pip3 install numpy
 # Install and build OpenCV
 wget -O ~/opencv-3.2.0.zip https://github.com/opencv/opencv/archive/3.2.0.zip
 unzip ~/opencv-3.2.0.zip
+wget -O ~/opencv_contrib-3.2.0.zip https://github.com/opencv/opencv_contrib/archive/3.2.0.zip
+unzip ~/opencv_contrib-3.2.0.zip
 cd ~/opencv-3.2.0
 mkdir build && cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D CMAKE_INSTALL_PREFIX=/usr/local \
 	-D INSTALL_C_EXAMPLES=OFF \
 	-D INSTALL_PYTHON_EXAMPLES=OFF \
-	-D BUILD_EXAMPLES=OFF ..
+	-D BUILD_EXAMPLES=OFF \
+        -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.2.0/modules ..
 make -j4
 sudo make install
 sudo ldconfig
