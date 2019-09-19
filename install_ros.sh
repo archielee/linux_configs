3083654d73477c5794bf17d997c1c84de974d218
Archie Lee <archielee@users.noreply.github.com>
2019-09-19 11:46:37 -0400

    Update ROS install script for Kinetic

--- install_ros.sh
+++ install_ros.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-while true; do
-    read -p "This installs ROS Indigo. Do you want to continue? " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+echo "Installing ROS Kinetic"
 
 # Add sources and keys
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 
 # Install ROS
-sudo apt-get update
-sudo apt-get install -y ros-indigo-desktop-full
+sudo apt update
+sudo apt install ros-kinetic-desktop-full
 sudo rosdep init
 rosdep update
 
 # Update ~/.bashrc
-echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 # Install rosinstall
-sudo apt-get install -y python-rosinstall
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools
